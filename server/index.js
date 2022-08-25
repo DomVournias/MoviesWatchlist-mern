846a2fa2b3b2584da1959bdf48a84bc49ee386e9
DomVournias <domvournias@gmail.com>
2022-08-26 01:23:55 +0300

    serve frontend

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use("/api", require("./routes/Film_router"));
 
 // const MODE = config.get("NODE_ENV");
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join("client/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "/", "client", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "client", "index.html"))
   );
 } else {
   app.get("/", (req, res) => res.send("Please set to production"));
